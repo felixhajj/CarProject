d00784fcbb275fe8cff31508400efe285ff32b45
Felix Hajj <felixhajj@gmail.com>
2024-10-29 19:00:14 +0200

    bit of updates

--- Car_Project/Assets/Car.cs
+++ Car_Project/Assets/Car.cs
@@ -7,6 +7,7 @@ using static Unity.VisualScripting.Dependencies.Sqlite.SQLite3;
 
 public class Car
 {
+    public string Name { get; set; }
     public float MaxTorque { get; set; } // Nm max torque out of crankshaft (engine torque)
     public AnimationCurve Torque { get; set; } // Torque curve coming out of the crankshaft
     public float BrakeTorque { get; set; }
@@ -24,11 +25,16 @@ public class Car
     public float WheelMass { get; set; } // Mass of the wheel in kg
 
 }
-
+//we do a builder to configure the properties
 public class CarBuilder
 {
     private Car _car= new Car();
 
+    public CarBuilder SetName(string name)
+    {
+        _car.Name = name;
+        return this;
+    }
     public CarBuilder SetMaxTorque(float maxTorque)
     {
         _car.MaxTorque = maxTorque;
@@ -99,19 +105,69 @@ public class CarBuilder
     {
         return _car;
     }
+
+    public Car Clone()
+    {
+        Car newCar = new Car();
+        newCar.Name = _car.Name;
+        newCar.MaxTorque = _car.MaxTorque;
+        newCar.Torque = new AnimationCurve(_car.Torque.keys); // Deep copy of AnimationCurve
+        newCar.BrakeTorque = _car.BrakeTorque;
+        newCar.HandBrakeTorque = _car.HandBrakeTorque;
+        newCar.SteeringMax = _car.SteeringMax;
+        newCar.MaxSpeed = _car.MaxSpeed;
+        newCar.SteeringCurve = new AnimationCurve(_car.SteeringCurve.keys); // Deep copy of AnimationCurve
+        newCar.TractionCutoff = _car.TractionCutoff;
+        newCar.FinalDriveAxle = _car.FinalDriveAxle;
+        newCar.DrivingWheels = _car.DrivingWheels;
+        newCar.Mass = _car.Mass;
+        newCar.WheelRadius = _car.WheelRadius;
+        newCar.WheelMass = _car.WheelMass;
+
+        return newCar;
+    }
+
+    
 }
 
-/*
+public class Inventory
+{
+    CarBuilder car;
+    public Inventory(CarBuilder carr)
+    {
+        car = carr;
+    }
 
-example usage:
-// Storing template
-Car sportsCarTemplate = new CarBuilder().SetMaxTorque(656).SetBrakeTorque(5000f).Build();
-carConfigs["SportsCar"] = sportsCarTemplate;
+    public Car createcar()
+    {
+        return car.Clone();
+    }
+}
+
+
+
+
+Car sportcar = new CarBuilder().SetBrakeTorque().Build();
+
+Inventory sportcarinv = new Inventory(sportcar);
+
+var sportcar2 = sportcarinv.createcar();
+
+
+
+
+//example usage:
+//// Storing template
+//Car sportsCarTemplate = new CarBuilder().SetMaxTorque(656).SetBrakeTorque(5000f).Build();
+//carConfigs["SportsCar"] = sportsCarTemplate;
 
 // Cloning the car
-Car newSportsCar = carConfigs["SportsCar"].Clone();
+//Car newSportsCar = carConfigs["SportsCar"].Clone();
 
-*/
+
+
+
+
 //private static readonly Dictionary<string, Car> carPresets = new Dictionary<string, Car>
 //    {
 //        { "SportsCar", new PlayerCar { MaxTorque = 656, BrakeTorque = 5000f, HandBrakeTorque = 3000f, SteeringMax = 27, MaxSpeed = 320f, TractionCutoff = 0.35f, FinalDriveAxle = 3.7f, DrivingWheels = 2, Mass = 1800, WheelRadius = 0.34f, WheelMass = 25f }},
